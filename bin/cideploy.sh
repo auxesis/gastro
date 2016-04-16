bd8ed01cb2473370e8023491423b264556b63bc0
Lindsay Holmwood <lindsay@holmwood.id.au>
2016-04-16 12:21:40 +1000

    Specify the Cloud Foundry API endpoint to use

--- bin/cideploy.sh
+++ bin/cideploy.sh
@@ -6,7 +6,7 @@ tar zxvf cf.tar.gz -C bin
 export PATH=$PATH:$(pwd)/bin
 
 # Login to cf (these environment variables must be exported by CI)
-cf login -u $CF_USERNAME -p $CF_PASSWORD -o $CF_ORG -s $CF_SPACE
+cf login -a $CF_API -u $CF_USERNAME -p $CF_PASSWORD -o $CF_ORG -s $CF_SPACE
 
 # Deploy the app
 cf push
