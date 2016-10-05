50b04b9b9d22289130132334d78d262c0f5b4087
Lindsay Holmwood <lindsay@holmwood.id.au>
2016-10-05 20:26:04 +1100

    Fix route for gotgastroagain.com, and set lower limits to lower the cost

--- bin/cideploy.sh
+++ bin/cideploy.sh
@@ -15,14 +15,14 @@ cf login -a $CF_API -u $CF_USERNAME -p $CF_PASSWORD -o $CF_ORG -s $CF_SPACE
 
 # Update the blue app
 cf unmap-route blue cfapps.io -n gotgastro
-cf unmap-route blue gotgastroagain.com -n gotgastro
-cf push blue --no-hostname --no-manifest --no-route -i 1 -m 128M
+cf unmap-route blue gotgastroagain.com
+cf push blue --no-hostname --no-manifest --no-route -i 1 -m 100M -k 200M
 cf map-route blue cfapps.io -n gotgastro
-cf map-route blue gotgastroagain.com -n gotgastro
+cf map-route blue gotgastroagain.com
 
 # Update the green app
 cf unmap-route green cfapps.io -n gotgastro
-cf unmap-route green gotgastroagain.com -n gotgastro
-cf push green --no-hostname --no-manifest --no-route -i 1 -m 128M
+cf unmap-route green gotgastroagain.com
+cf push green --no-hostname --no-manifest --no-route -i 1 -m 100M -k 200M
 cf map-route green cfapps.io -n gotgastro
-cf map-route green gotgastroagain.com -n gotgastro
+cf map-route green gotgastroagain.com
