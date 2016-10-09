f4551596f918d37f96a2a090be702a30ca9a1043
Lindsay Holmwood <lindsay@holmwood.id.au>
2016-10-09 21:14:59 +1100

    Version apps in one place, to improve traceability

--- bin/cideploy.sh
+++ bin/cideploy.sh
@@ -20,13 +20,13 @@ set -x
 # Update the blue app
 cf unmap-route blue cfapps.io -n gotgastro
 cf unmap-route blue gotgastroagain.com
-cf push blue --no-hostname --no-manifest --no-route -i 1 -m 100M -k 200M
+cf push blue -f manifest.yml
 cf map-route blue cfapps.io -n gotgastro
 cf map-route blue gotgastroagain.com
 
 # Update the green app
 cf unmap-route green cfapps.io -n gotgastro
 cf unmap-route green gotgastroagain.com
-cf push green --no-hostname --no-manifest --no-route -i 1 -m 100M -k 200M
+cf push green -f manifest.yml
 cf map-route green cfapps.io -n gotgastro
 cf map-route green gotgastroagain.com
