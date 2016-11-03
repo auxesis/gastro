d4da5ec52440756cee20b54a9b12dafcedb2f6eb
Lindsay Holmwood <lindsay@holmwood.id.au>
2016-11-03 12:38:43 +1100

    Add ability to require an absolute URL, and order JavaScript load:
     - Include vendored common content first, so libraries are in place
       before other page-specific JavaScript is loaded.
     - Then include page-specific JavaScript, so the libraries are already
       in place, and they don't break.

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -122,14 +122,21 @@ module Sinatra
 
     def include_required_js
       if @js_filenames
-        @js_filenames.map { |filename, opts|
+        @js_filenames.sort_by {|f,o| f =~ /^vendor/ ? 0 : 1 }.map { |filename, opts|
           type = opts[:type] ? opts[:type] : 'text/javascript'
-          %(<script src="#{link_to("/js/#{filename}.js")}" type="#{type}"></script>)
+          # if the path is absolute, insert in directly
+          if filename =~ /^http/
+            attrs = opts.map{|k,v| %(#{k}="#{v}")}.join(' ')
+            %(<script src="#{filename}" type="#{type}" #{attrs}></script>)
+          else
+            %(<script src="#{link_to("/js/#{filename}.js", :asset => true)}" type="#{type}"></script>)
+          end
         }.join("\n")
       else
         ""
       end
     end
+
   end
 
   module RequireCSSHelper
