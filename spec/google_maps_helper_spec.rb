94793a56a8ea2e1790cda4d463874fbc68cd42c9
Lindsay Holmwood <lindsay@holmwood.id.au>
2016-11-20 21:35:47 +1100

    Test that duplicate markers aren't generated

--- spec/google_maps_helper_spec.rb
+++ spec/google_maps_helper_spec.rb
@@ -13,12 +13,31 @@ describe 'GoogleMapsHelpers' do
   end
 
   it 'should ensure the url is no longer than 2000 characters' do
-    thousands_of_results
+    thousands_of_results && some_prosecutions
 
     businesses = Business.all
     gmaps_api_key = config['settings']['gmaps_api_key']
 
     map_url = google_map(:api_key => gmaps_api_key, :businesses => businesses, :location => origin)
     expect(map_url.size).to be <= 2000
+  end
+
+  it 'should not include duplicate locations in separate marker lists' do
+    thousands_of_results && some_prosecutions
+
+    businesses = Business.all
+    gmaps_api_key = config['settings']['gmaps_api_key']
+    map_url = google_map(:api_key => gmaps_api_key, :businesses => businesses, :location => origin)
+
+    # Parse the url into an array of markers
+    url = Addressable::URI.parse(map_url)
+    markers = url.query_values(Array).select {|k, v| k == 'markers' }.map {|k,v| v}
+
+    # Build up a list of all the locations specified for markers
+    locations = []
+    markers.each { |marker| locations.concat(marker.split('|')[2..-1]) }
+
+    # Check they're uniq
+    expect(locations.uniq.size).to eq(locations.size)
   end
 end
