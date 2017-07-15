b004b8cb755c4f0fb9de3a5a4534c38dd19ad873
Lindsay Holmwood <lindsay@holmwood.id.au>
2017-07-15 22:54:23 +1000

    Move duck punch into more appropriate file

--- lib/gotgastro/initializer.rb
+++ lib/gotgastro/initializer.rb
@@ -128,31 +128,6 @@ end
 # http://osdir.com/ml/sequel-talk/2012-01/msg00076.html
 require 'gotgastro/models'
 
-# Add dataset method to return points ordered by distance from an origin
-module Sequel
-  module Plugins
-    module Mappable
-      module DatasetMethods
-        def by_distance(origin)
-          sql = model.distance_sql(origin).lit
-          self.select_append {
-            Sequel::SQL::AliasedExpression.new(sql.lit, :distance)
-          }.order(:distance)
-        end
-        def around(origin, distance)
-          sql = model.distance_sql(origin)
-          f_origin_bbox(origin, distance)
-          self.select_append {
-            Sequel::SQL::AliasedExpression.new(sql.lit, :distance)
-          }
-          .order(:distance)
-          .filter{sql.lit <= distance}
-        end
-      end
-    end
-  end
-end
-
 # Initialise services for handling data
 require 'gotgastro/services'
 
--- lib/gotgastro/models.rb
+++ lib/gotgastro/models.rb
@@ -10,3 +10,29 @@ Sequel::Model.plugin :table_select
 models_path = Pathname.new(__FILE__).parent.join('models').join('*.rb')
 models = Dir.glob(models_path)
 models.each { |model| require model }
+
+# Add dataset method to return points ordered by distance from an origin
+module Sequel
+  module Plugins
+    module Mappable
+      module DatasetMethods
+        def by_distance(origin)
+          sql = model.distance_sql(origin).lit
+          self.select_append {
+            Sequel::SQL::AliasedExpression.new(sql.lit, :distance)
+          }.order(:distance)
+        end
+        def around(origin, distance)
+          sql = model.distance_sql(origin)
+          f_origin_bbox(origin, distance)
+          self.select_append {
+            Sequel::SQL::AliasedExpression.new(sql.lit, :distance)
+          }
+          .order(:distance)
+          .filter{sql.lit <= distance}
+        end
+      end
+    end
+  end
+end
+
